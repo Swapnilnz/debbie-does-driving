972695f7411e14e7e79d3a3396d0ac71b6e847ee
Swapnil Bhagat <sbh94@uclive.ac.nz>
2021-11-05 12:44:36 +1300

    Added image blur load

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,11 @@ import "../css/Home.css"
 import {LogoAndWord} from './LogoSVG';
 import HeroImgCropped from '../assets/hero_cropped.png';
 import HeroImg from '../assets/hero.png';
-import RoadSample from '../assets/road-sample.jpg'
+import BlurredHeroImg from '../assets/blurred_hero.png';
+import BlurredHeroCropped from '../assets/blurred_hero_cropped.png'
+import RoadSample from '../assets/road-sample.jpg';
+import BlurredRoadSample from '../assets/blurred_road_sample.jpg';
+import BlurImageLoader from 'react-blur-image-loader';
 import { useMediaQuery } from 'react-responsive';
 import Button from '@mui/material/Button';
 import LocalLibraryTwoToneIcon from '@mui/icons-material/LocalLibraryTwoTone';
@@ -19,8 +23,8 @@ function Home() {
     return (
     <div className="home-wrapper">
         <div className="hero">
-            {isDesktopOrLaptop && <img src={HeroImgCropped} className="hero-img" alt="debbie"/>}
-            {isTabletOrMobile && <img src={HeroImg} className="hero-img" alt="debbie"/>}
+            {isDesktopOrLaptop && <BlurImageLoader src={HeroImgCropped} preview={BlurredHeroCropped} fullCover={true} transitionTime={100}/>}
+            {isTabletOrMobile && <BlurImageLoader src={HeroImg} preview={BlurredHeroImg} fullCover={true} transitionTime={100}/>}
             <div className="wave">
                 <svg className="shadow" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
                     <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" className="shape-fill"/>
@@ -29,7 +33,7 @@ function Home() {
             <div className="hero-overlay">
                 <div>
                     {isDesktopOrLaptop && <LogoAndWord width="450" height="150"/>}
-                    {isTabletOrMobile && <LogoAndWord width="200" height="90" />}
+                    {isTabletOrMobile && <LogoAndWord width="200" height="150" />}
                 </div>
                 <div className="has-text-white my-3 has-text-centered overlay-contact">
                     <p>debbiedoesdriving@gmail.com</p>
@@ -55,7 +59,7 @@ function Home() {
         <div className="spacer layered-waves-1"/>
         <div className="meet-debbie-wrapper">
             {isDesktopOrLaptop && <div className="p-4">
-                <img src={RoadSample} width={300} height={900} className="meet-debbie-image" alt="road"/>
+                <BlurImageLoader src={RoadSample} preview={BlurredRoadSample} fullCover={true} transitionTime={100}/>
             </div>}
             <div className="meet-debbie-body p-4">
                 <div className="has-text-weight-bold has-text-white meet-debbie-header is-flex is-flex-direction-row">
