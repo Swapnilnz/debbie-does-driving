58c4e777c5b37cde51c861a74de6b4c6e3107cd2
Raven Townsend <rto45@uclive.ac.nz>
2021-11-21 15:35:37 +1300

    Update location

--- src/components/contact/LocationDetails.js
+++ src/components/contact/LocationDetails.js
@@ -27,11 +27,8 @@ function GetInTouch() {
             <div className="home-section-subtitle mb-1">
                 MOTORCYCLE TRAINING
             </div>
-            <div className="address-line"
-               href="https://www.google.com/maps/place/14a+Ballantyne+Avenue,+Upper+Riccarton,+Christchurch+8041/@-43.5345183,172.5688385,17z/data=!3m1!4b1!4m5!3m4!1s0x6d318bbacfdcd7c9:0x86366377bcc937ab!8m2!3d-43.5345183!4d172.5710272"
-               target="_blank"
-               rel="noopener noreferrer">
-                Your place, Invercargill
+            <div className="address-line">
+                We come to you, Invercargill
             </div>
 
         </div>
