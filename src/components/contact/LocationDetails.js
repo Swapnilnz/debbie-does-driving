4df8c4fe6c3aa54dc3c3648966d1223a06bbf1bb
Raven Townsend <rto45@uclive.ac.nz>
2021-11-21 15:34:46 +1300

    Update locations

--- src/components/contact/LocationDetails.js
+++ src/components/contact/LocationDetails.js
@@ -17,28 +17,22 @@ function GetInTouch() {
             <div className="home-section-subtitle mb-1">
                 DEFENSIVE DRIVER COURSE
             </div>
-            <a className="address-line"
-               href="https://www.google.com/maps/place/14a+Ballantyne+Avenue,+Upper+Riccarton,+Christchurch+8041/@-43.5345183,172.5688385,17z/data=!3m1!4b1!4m5!3m4!1s0x6d318bbacfdcd7c9:0x86366377bcc937ab!8m2!3d-43.5345183!4d172.5710272"
+            <a className="address-line-hover"
+               href="https://https://www.google.com/maps/place/Adventure+Southland+Limited/@-46.3505345,168.3443628,17z/data=!3m1!4b1!4m5!3m4!1s0xa9d2dc89f80adb79:0x8751bd7a03c88adf!8m2!3d-46.3505345!4d168.3465503"
                target="_blank"
                rel="noopener noreferrer">
                 Invercargill
-            </a>
-            <a className="address-line"
-               href="https://www.google.com/maps/place/14a+Ballantyne+Avenue,+Upper+Riccarton,+Christchurch+8041/@-43.5345183,172.5688385,17z/data=!3m1!4b1!4m5!3m4!1s0x6d318bbacfdcd7c9:0x86366377bcc937ab!8m2!3d-43.5345183!4d172.5710272"
-               target="_blank"
-               rel="noopener noreferrer">
-                Hamilton
             </a>
 
             <div className="home-section-subtitle mb-1">
                 MOTORCYCLE TRAINING
             </div>
-            <a className="address-line"
+            <div className="address-line"
                href="https://www.google.com/maps/place/14a+Ballantyne+Avenue,+Upper+Riccarton,+Christchurch+8041/@-43.5345183,172.5688385,17z/data=!3m1!4b1!4m5!3m4!1s0x6d318bbacfdcd7c9:0x86366377bcc937ab!8m2!3d-43.5345183!4d172.5710272"
                target="_blank"
                rel="noopener noreferrer">
-                Invercargill
-            </a>
+                Your place, Invercargill
+            </div>
 
         </div>
     )
