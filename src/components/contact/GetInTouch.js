3d820917d4fad35e4629a324a5045fef11fb0506
Swapnil Bhagat <sbh94@uclive.ac.nz>
2023-03-31 23:08:46 +1100

    Add form options as needed

--- src/components/contact/GetInTouch.js
+++ src/components/contact/GetInTouch.js
@@ -3,10 +3,15 @@ import React, {useState} from 'react';
 
 import {Alert, Button, InputAdornment, Snackbar, TextField} from "@mui/material";
 import {withStyles} from "@material-ui/core/styles";
-import {Email, Phone, Send} from "@mui/icons-material";
+import {Badge, Email, Phone, Send} from "@mui/icons-material";
 import {useMediaQuery} from "react-responsive";
 import {useForm} from "react-hook-form";
 import emailjs from "emailjs-com";
+import TabContext from "@mui/lab/TabContext";
+import Box from "@mui/material/Box";
+import TabList from "@mui/lab/TabList";
+import Tab from "@mui/material/Tab";
+import TabPanel from "@mui/lab/TabPanel";
 
 
 emailjs.init("user_g9lWclMvGS8QwiE769BVV");
@@ -23,6 +28,11 @@ const styles = {
 function GetInTouch(props) {
     const {classes} = props;
     const isDesktopOrLaptop = useMediaQuery({query: '(min-width: 1224px)'});
+    const [tab, setTab] = useState('1');
+
+    const handleChange = (event, newValue) => {
+        setTab(newValue);
+    };
 
     // Set snackbar variables
     const [openSnackbar, setOpenSnackbar] = useState(false);
@@ -41,23 +51,34 @@ function GetInTouch(props) {
 
     const onSubmit = function (data) {
         try {
+            const isMCycle = tab === "1"
+            console.log(isMCycle)
             const date = Date().toLocaleString().split(" GMT")[0]; // removes the GMT+1300... bit at the end
 
             console.log(data)
-            const message = data.message.replace(/(?:\r\n|\r|\n)/g, '<br>');
+            const message = isMCycle ? "N/A" : data.message.replace(/(?:\r\n|\r|\n)/g, '<br>');
+            const experience = isMCycle ? data.experience.replace(/(?:\r\n|\r|\n)/g, '<br>') : "N/A";
+            const goals = isMCycle ? data.goals.replace(/(?:\r\n|\r|\n)/g, '<br>') : "N/A";
             emailjs.send(emailjsService, emailjsTemplateToDebbie, {
                 date: date,
                 name: data.name,
                 email: data.email,
-                number: data.number,
+                phone: data.number,
                 message: message,
+                lclass: isMCycle ? data.lclass : "N/A",
+                experience: experience,
+                goals: goals
             });
             emailjs.send(emailjsService, emailjsTemplateToCustomer, {
                 date: date,
                 name: data.name,
                 email: data.email,
-                number: data.number,
-                message: message,
+                lclass: data.lclass,
+                phone: data.number,
+                message: isMCycle ?
+                    `Experience:\n${experience}\n
+                    Goals:\n${goals}
+                    ` : message,
             });
             setSeverity("warning");
             setMessage("Contact form submitted");
@@ -65,7 +86,10 @@ function GetInTouch(props) {
             reset({
                 name: '',
                 email: '',
-                message: ''
+                message: '',
+                lclass: '',
+                experience: '',
+                goals: ''
             })
         } catch (err) {
             console.error(err);
@@ -84,69 +108,183 @@ function GetInTouch(props) {
             <div className="home-section-subtitle mb-5">
                 · got a question? ·
             </div>
-            <form
-                onSubmit={handleSubmit(onSubmit)}
-                className="is-flex is-flex-direction-column git-body">
-                <TextField
-                    fullWidth
-                    {...register("name", {required: true})}
-                    InputProps={{className: classes.input}}
-                    required
-                    color={"warning"}
-                    focused
-                    label="Name"
-                />
-                <TextField
-                    fullWidth
-                    {...register("email", {required: true})}
-                    InputProps={{
-                        className: classes.input,
-                        startAdornment: (
-                            <InputAdornment position={"start"}>
-                                <Email color={"warning"}/>
-                            </InputAdornment>
-                        )
-                    }}
-                    required
-                    color={"warning"}
-                    focused
-                    type={"email"}
-                    label="Email"
-                />
+            <div className={"git-body is-flex is-flex-direction-column"}>
+                <TabContext value={tab}>
+                    <Box sx={{borderBottom: 1, zIndex: 2, alignSelf: "center"}}>
+                        <TabList onChange={handleChange} textColor="white" indicatorColor={"secondary"}>
+                            <Tab label="Motorcycle" value="1" sx={{color: tab === "1" ? "white" : "#a5a5a5"}}/>
+                            <Tab label="General" value="2" sx={{color: tab === "2" ? "white" : "#a5a5a5"}}/>
+                        </TabList>
+                    </Box>
+                    <TabPanel value={"1"}>
+                        <form
+                            onSubmit={handleSubmit(onSubmit)}
+                            className="is-flex is-flex-direction-column" style={{gap: "1rem"}}>
+                            <TextField
+                                fullWidth
+                                {...register("name", {required: true})}
+                                InputProps={{className: classes.input}}
+                                required
+                                color={"warning"}
+                                focused
+                                label="Name"
+                                inputProps={{maxLength: 20}}
+                            />
+                            <TextField
+                                fullWidth
+                                {...register("email", {required: true})}
+                                InputProps={{
+                                    className: classes.input,
+                                    startAdornment: (
+                                        <InputAdornment position={"start"}>
+                                            <Email color={"warning"}/>
+                                        </InputAdornment>
+                                    )
+                                }}
+                                required
+                                color={"warning"}
+                                focused
+                                type={"email"}
+                                label="Email"
+                                inputProps={{maxLength: 50}}
+                            />
 
-                <TextField
-                    fullWidth
-                    {...register("number", {required: true})}
-                    InputProps={{
-                        className: classes.input,
-                        startAdornment: (
-                            <InputAdornment position={"start"}>
-                                <Phone color={"warning"}/>
-                            </InputAdornment>
-                        )
-                    }}
-                    required
-                    color={"warning"}
-                    focused
-                    type={"number"}
-                    label="Phone No."
-                />
-                <TextField
-                    {...register("message", {required: true})}
-                    label="Message"
-                    required
-                    multiline
-                    rows={4}
-                    InputProps={{className: classes.input}}
-                    color={"warning"}
-                    focused
-                />
-                <Button variant="contained" size={isDesktopOrLaptop ? 'large' : 'small'}
-                        type={"submit"}
-                        className="orange-button has-text-weight-bold" startIcon={<Send/>}>
-                    Send
-                </Button>
-            </form>
+                            <TextField
+                                fullWidth
+                                {...register("number", {required: true})}
+                                InputProps={{
+                                    className: classes.input,
+                                    startAdornment: (
+                                        <InputAdornment position={"start"}>
+                                            <Phone color={"warning"}/>
+                                        </InputAdornment>
+                                    )
+                                }}
+                                required
+                                color={"warning"}
+                                focused
+                                type={"number"}
+                                label="Phone No."
+                                inputProps={{max: 100000000000}}
+                            />
+
+                            <TextField
+                                fullWidth
+                                {...register("lclass", {required: true})}
+                                InputProps={{
+                                    className: classes.input,
+                                    startAdornment: (
+                                        <InputAdornment position={"start"}>
+                                            <Badge color={"warning"}/>
+                                        </InputAdornment>
+                                    )
+                                }}
+                                required
+                                color={"warning"}
+                                focused
+                                label="License Class"
+                                inputProps={{maxLength: 20}}
+                            />
+                            <TextField
+                                {...register("experience", {required: true})}
+                                label="Experience"
+                                required
+                                multiline
+                                rows={4}
+                                InputProps={{className: classes.input}}
+                                color={"warning"}
+                                focused
+                                inputProps={{maxLength: 300}}
+                            />
+                            <TextField
+                                {...register("goals", {required: true})}
+                                label="Goals"
+                                required
+                                multiline
+                                rows={4}
+                                InputProps={{className: classes.input}}
+                                color={"warning"}
+                                focused
+                                inputProps={{maxLength: 300}}
+                            />
+                            <Button variant="contained" size={isDesktopOrLaptop ? 'large' : 'small'}
+                                    type={"submit"}
+                                    className="orange-button has-text-weight-bold" startIcon={<Send/>}>
+                                Send
+                            </Button>
+                        </form>
+                    </TabPanel>
+                    <TabPanel value={"2"}>
+                        <form
+                            onSubmit={handleSubmit(onSubmit)}
+                            className="is-flex is-flex-direction-column" style={{gap: "1rem"}}>
+                            <TextField
+                                fullWidth
+                                {...register("name", {required: true})}
+                                InputProps={{className: classes.input}}
+                                required
+                                color={"warning"}
+                                focused
+                                label="Name"
+                                inputProps={{maxLength: 20}}
+                            />
+                            <TextField
+                                fullWidth
+                                {...register("email", {required: true})}
+                                InputProps={{
+                                    className: classes.input,
+                                    startAdornment: (
+                                        <InputAdornment position={"start"}>
+                                            <Email color={"warning"}/>
+                                        </InputAdornment>
+                                    )
+                                }}
+                                required
+                                color={"warning"}
+                                focused
+                                type={"email"}
+                                label="Email"
+                                inputProps={{maxLength: 50}}
+
+                            />
+
+                            <TextField
+                                fullWidth
+                                {...register("number", {required: true})}
+                                InputProps={{
+                                    className: classes.input,
+                                    startAdornment: (
+                                        <InputAdornment position={"start"}>
+                                            <Phone color={"warning"}/>
+                                        </InputAdornment>
+                                    )
+                                }}
+                                required
+                                color={"warning"}
+                                focused
+                                type={"number"}
+                                label="Phone No."
+                                inputProps={{max: 100000000000}}
+                            />
+                            <TextField
+                                {...register("message", {required: false})}
+                                label="Message"
+                                required
+                                multiline
+                                rows={4}
+                                InputProps={{className: classes.input}}
+                                color={"warning"}
+                                focused
+                                inputProps={{maxLength: 500}}
+                            />
+                            <Button variant="contained" size={isDesktopOrLaptop ? 'large' : 'small'}
+                                    type={"submit"}
+                                    className="orange-button has-text-weight-bold" startIcon={<Send/>}>
+                                Send
+                            </Button>
+                        </form>
+                    </TabPanel>
+                </TabContext></div>
 
             <Snackbar
                 open={openSnackbar}
