282741c44537a1043d8e18d26ca6fab407cb626d
Swapnil Bhagat <sbh94@uclive.ac.nz>
2023-05-02 12:49:41 +1000

    Disable motorcycle

--- src/views/Motorcycle.js
+++ src/views/Motorcycle.js
@@ -1,23 +1,28 @@
 import '../css/motorcycle/Motorcycle.css';
-import Learners from "../components/motorcycle/learners/Learners";
-import Restricted from "../components/motorcycle/restricted/Restricted";
-import Full from "../components/motorcycle/Full";
-import L2 from '../assets/layered-waves-2.svg';
-import L3 from '../assets/layered-waves-3.svg';
+import {Alert} from "@mui/material";
 
 function Motorcycle() {
 
     return (
         <div className="pd-7 is-flex is-justify-content-center is-flex-direction-column pb-5 " id="learners">
-            <Learners/>
-            <div className="spacer" id="restricted">
-                <img loading={'lazy'} src={L2} width={'100%'} alt={'divider'}/>
-            </div>
-            <Restricted/>
-            <div className="spacer" id="full">
-                <img loading={'lazy'} src={L3} width={'100%'} alt={'divider'}/>
-            </div>
-            <Full/>
+            <Alert severity="info" style={{width: "90%", alignSelf: "center"}}>
+                Debbie is super pleased to announce she is now providing motorcycle training as a member of the <strong>Love
+                Moto</strong> team in Invercargill.
+                <br/>
+                For all your Motorcycle Training & Licensing needs, please contact Love Moto – phone: 0220336445 |
+                email: info@lovemoto.co.nz | website: <a target={"_blank"} rel="noreferrer"
+                                                         href={"https://lovemoto.co.nz/"}>https://lovemoto.co.nz/</a>
+            </Alert>
+
+            {/*<Learners/>*/}
+            {/*<div className="spacer" id="restricted">*/}
+            {/*    <img loading={'lazy'} src={L2} width={'100%'} alt={'divider'}/>*/}
+            {/*</div>*/}
+            {/*<Restricted/>*/}
+            {/*<div className="spacer" id="full">*/}
+            {/*    <img loading={'lazy'} src={L3} width={'100%'} alt={'divider'}/>*/}
+            {/*</div>*/}
+            {/*<Full/>*/}
         </div>
     )
 }
