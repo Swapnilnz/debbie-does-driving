65d819995535b621113d06cd9903930377c63478
Swapnil Bhagat <swapnil.bhagat.nz@gmail.com>
2023-05-25 17:23:36 +1000

    update reviews

--- src/components/home/ReviewData.js
+++ src/components/home/ReviewData.js
@@ -1,44 +1,50 @@
+// Random color generator
+export const randomColor = () => {
+    const colors = ['#d56d2d', '#d5a52d', '#d52d2d', '#2d6dd5', '#2dd5d5', '#2dd55e', '#d52dd5', '#d52d6d', '#2dd5a5'];
+    return colors[Math.floor(Math.random() * colors.length)];
+};
+
 export const reviewData = [
+    {
+        name: 'Anonymous',
+        fname: 'Anonymous',
+        bg: randomColor(),
+        message: 'I attended the Insight 3 course recently,  Debbie was absolutely fantastic.  She is not only supportive but extremely professional. I\'ve learnt so much from her.  She has been of great assistance to me and I would highly recommend her ',
+    },
     {
         name: 'Tom Kahu',
         fname: 'Tom',
-        bg: '#d56d2d',
-        avatar: 'https://scontent.fwlg3-1.fna.fbcdn.net/v/t39.30808-1/cp0/p60x60/231194656_4587711204572540_1026354474099227966_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=7206a8&_nc_ohc=ef2BB30r_k0AX8tH6Wu&_nc_ht=scontent.fwlg3-1.fna&oh=01aae23a37ee10eec08d59f0f2080e0f&oe=618EDBF9',
+        bg: randomColor(),
         message: 'Highly recommend Debz for your bike license or any other kind of training to move towards what ever goal you\'re heading for, awesome lady, very professional with her lessons and very informative. I’ll see you in 9 months for my full ride thanks again…'
     },
     {
         name: 'Jenn Hooper',
         fname: 'Jenn',
-        bg: '#d56d2d',
-        avatar: 'https://scontent.fwlg3-1.fna.fbcdn.net/v/t1.6435-1/cp0/p60x60/244467265_10158262746826881_8023994240980878780_n.jpg?_nc_cat=104&ccb=1-5&_nc_sid=7206a8&_nc_ohc=NRp3lC-fe3wAX-AjPBV&_nc_ht=scontent.fwlg3-1.fna&oh=1e0f739d4f9a4578fdd3308373059c11&oe=61AF8961',
+        bg: randomColor(),
         message: 'Super knowledgeable and supportive. If you want a gentle hand that knows her stuff, Debbie’s your Gal! Thanks so much x',
     },
     {
         name: 'Runsin Jacob',
         fname: 'Runsin',
-        bg: '#d56d2d',
-        avatar: 'https://scontent.fwlg3-1.fna.fbcdn.net/v/t1.6435-1/cp0/p60x60/176190624_4176659205697995_4039213119677748704_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=7206a8&_nc_ohc=BQU3rYf7_F0AX-jkPMO&_nc_ht=scontent.fwlg3-1.fna&oh=8acdf31b31a5aae422b8b330d7b8c373&oe=61AE2E34',
+        bg: randomColor(),
         message: 'the best rider instructor ever, very supportive, motivative, well experienced...',
     },
     {
         name: 'Stu Craig',
         fname: 'Stu',
-        bg: '#d56d2d',
-        avatar: 'https://scontent.fwlg3-1.fna.fbcdn.net/v/t39.30808-1/cp0/c9.476.653.652a/s60x60/243889864_4339042249525103_2163751152011049013_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=7206a8&_nc_ohc=N7xMsm-5v8cAX8RtCYn&_nc_ht=scontent.fwlg3-1.fna&oh=4746fa135cd8f6a485e1584ea7130056&oe=618E9AC3',
+        bg: randomColor(),
         message: 'I did the 6F cbta course with Debbie, I fully recommend Debbie\'s services to anyone getting there bike licence, Debbie made the whole assessment comfortable friendly and honest, offered get advice before the assessment, and excellent helpful tips for improvement after the assessment, a no pressure friendly enjoyable service',
     },
     {
         name: 'Dulani Dissanayaka',
         fname: 'Dulani',
-        bg: '#d56d2d',
-        avatar: 'https://lh3.googleusercontent.com/a/AATXAJx1rq0ldAiqofVRjxE6pKnLUiol2e_Qh1pZ18Fl=w83-h83-p-rp-mo-br100',
+        bg: randomColor(),
         message: 'Very professional, quality and very valuable lessons she provide and highly recommend.',
     },
     {
         name: 'Nadeem Daraghmeh',
         fname: 'Nadeem',
-        bg: '#d56d2d',
-        avatar: 'https://lh3.googleusercontent.com/a/AATXAJx5DyIc9rTxDd0vb2Aj7QFJDkpscmHcJfKE49U_=w83-h83-p-rp-mo-br100',
+        bg: randomColor(),
         message: 'Professional, supportive and lots of experience, highly recommended.',
     }
 ]
