5d265690bc6e71b6f05c424ce67ef09bb509abde
Swapnil Bhagat <swapnil.bhagat.nz@gmail.com>
2023-07-07 13:18:03 +1000

    remove insight3

--- src/components/car/Retraining.js
+++ src/components/car/Retraining.js
@@ -1,14 +1,15 @@
 import {useMediaQuery} from "react-responsive";
 import RetrainingImage from "../../assets/retraining.jpg";
-import PriceSVG from '../../assets/265.svg';
-import {Typography} from "@mui/material";
-import {insight3} from "../../utilities/text";
-import BookButton from "../BookButton";
+// import PriceSVG from '../../assets/265.svg';
+// import {Typography} from "@mui/material";
+// import {insight3} from "../../utilities/text";
+// import BookButton from "../BookButton";
+import {Alert} from '@mui/material';
 
 function Retraining() {
 
     const isDesktopOrLaptop = useMediaQuery({query: '(min-width: 1224px)'});
-    const Price = PriceSVG;
+    // const Price = PriceSVG;
 
     return (
         <div className="retraining-wrapper">
@@ -24,7 +25,10 @@ function Retraining() {
                 <div className="home-section-subtitle mb-5">
                     · INSIGHT3 ·
                 </div>
-                <div className="has-text-white is-flex-grow-1">
+                <Alert severity="warning" style={{width: "100%", marginBottom: 15}}>
+                    This service is currently unavailable.
+                </Alert>
+                {/* <div className="has-text-white is-flex-grow-1">
                     <div className="car-price-image has-text-centered">
                         <img loading={'lazy'} src={Price} height={150} width={150} alt={'price 195'}/>
                     </div>
@@ -33,7 +37,7 @@ function Retraining() {
                     </Typography>
                 </div>
                 <BookButton
-                    link={'https://booking.setmore.com/scheduleappointment/7ce91070-a226-4451-b4a7-574d47cafe44/services/f60ce61c-fb3e-4098-9a9b-2d06d550c294'}/>
+                    link={'https://booking.setmore.com/scheduleappointment/7ce91070-a226-4451-b4a7-574d47cafe44/services/f60ce61c-fb3e-4098-9a9b-2d06d550c294'}/> */}
             </div>
         </div>
     );
--- src/components/home/Retraining.js
+++ src/components/home/Retraining.js
@@ -1,11 +1,12 @@
 import '../../css/home/Retraining.css';
 import RetrainingImage from "../../assets/retraining.jpg";
-import Button from "@mui/material/Button";
-import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
+// import Button from "@mui/material/Button";
+// import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
 import {useMediaQuery} from "react-responsive";
-import {HashLink as Link} from 'react-router-hash-link';
-import {insight3} from "../../utilities/text";
-import {Typography} from "@mui/material";
+// import {HashLink as Link} from 'react-router-hash-link';
+// import {insight3} from "../../utilities/text";
+// import {Typography} from "@mui/material";
+import {Alert} from '@mui/material';
 
 function Retraining() {
 
@@ -25,16 +26,21 @@ function Retraining() {
                 <div className="home-section-subtitle">
                     · INSIGHT3 ·
                 </div>
-                <Typography className="retraining-text mt-5 is-flex-grow-1">
+                <Alert severity="warning" style={{width: "100%", marginTop: 15}}>
+                    This service is currently unavailable.
+                </Alert>
+                {/* <Typography className="retraining-text mt-5 is-flex-grow-1">
                     {insight3.bodySmall}
-                </Typography>
+                </Typography> */}
                 <div className="retraining-buttons-wrapper mt-5">
-                    <Link to={"/car#retraining"}>
+                    {/* <Link to={"/car#retraining"}>
                         <Button variant="contained" size={isDesktopOrLaptop ? 'large' : 'small'}
                                 className="has-text-weight-bold more-info" startIcon={<InfoOutlinedIcon/>}>
                             More Info
                         </Button>
-                    </Link>
+                    </Link> */}
+                    
+
                 </div>
             </div>
         </div>
